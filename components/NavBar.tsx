8114001ecbf6c7efbb4a0f2cbdd7db7dcdb47862
unknown <singo55singo@gmail.com>
2026-04-27 03:55:52 +0900

    Write privacy policy

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -115,10 +115,10 @@ export default function NavBar() {
               )}
             </div>
             <div className="menuBttomLink" style={{position: 'absolute', bottom: '20px', fontSize: '16px',lineHeight: '35px'}}>
-              <a href="">漫画リレーについて</a><br/>
-              <a href="">利用ルール</a><br/>
+              <a href="https://github.com/tororo-kombu/manga-relay/blob/main/README.md#%E6%BC%AB%E7%94%BB%E3%83%AA%E3%83%AC%E3%83%BC-%E3%81%AB%E3%81%A4%E3%81%84%E3%81%A6">漫画リレーについて</a><br/>
+              <a href="https://github.com/tororo-kombu/manga-relay/blob/main/README.md#%EF%B8%8F%E5%88%A9%E7%94%A8%E4%B8%8A%E3%81%AE%E3%83%AB%E3%83%BC%E3%83%AB">利用ルール</a><br/>
               <a href="">公式X</a><br/>
-              <a href="">GitHub</a><br/>
+              <a href="https://github.com/tororo-kombu/manga-relay/tree/main">GitHub</a><br/>
               <a href="">プライバシーポリシー</a>
             </div>
           </div>
